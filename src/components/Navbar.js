29b63ddfa8fb98f2cf26a60de7d9bf4130b3c547
Piyush Mehta <me@piyushmehta.com>
2022-01-17 15:31:27 -0500

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,18 @@
 import React from "react";
+import styled from "styled-components";
 
+const Container = styled.div`
+  height: 60px;
+`;
+const Wrapper = styled.div`
+  padding: 10px 20px;
+`;
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <Container>
+      <Wrapper>Navbar</Wrapper>
+    </Container>
+  );
 };
 
 export default Navbar;

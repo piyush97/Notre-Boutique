54d71a81899c64294e713e0342c35d45da9e10b2
Piyush Mehta <me@piyushmehta.com>
2022-01-17 15:48:05 -0500

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,4 @@
+import { Search } from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
 
@@ -6,11 +7,41 @@ const Container = styled.div`
 `;
 const Wrapper = styled.div`
   padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+const Right = styled.div`
+  flex: 1;
+`;
+const Center = styled.div`
+  flex: 1;
+`;
+const Language = styled.span`
+  font-size: 14px;
+`;
+const SearchContainer = styled.div`
+  border: 1px solid lightgray;
+  display: flex;
+  align-items: center;
 `;
 const Navbar = () => {
   return (
     <Container>
-      <Wrapper>Navbar</Wrapper>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Search />
+          </SearchContainer>
+        </Left>
+        <Center>Center</Center>
+        <Right>Right</Right>
+      </Wrapper>
     </Container>
   );
 };

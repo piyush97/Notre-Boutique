1a03c895976eaf1f37efaeaacefce0ea38fc75bc
piyush97 <me@piyushmehta.com>
2022-01-19 17:28:27 -0500

    Slider

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,28 @@
 const APP_NAME = "Notre Boutique";
 const ANOUNCEMENT = "Super Deal! Free Shipping on Orders Over $50";
-export { APP_NAME, ANOUNCEMENT };
+
+const SLIDER_ITEMS = [
+  {
+    id: 1,
+    img: "https://i.ibb.co/XsdmR2c/1.png",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/DG69bQ4/2.png",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/cXFnLLV/3.png",
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
+
+export { APP_NAME, ANOUNCEMENT, SLIDER_ITEMS };

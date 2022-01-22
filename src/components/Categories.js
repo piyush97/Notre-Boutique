51ffbfe50d5226392bf62585f4255283e33dd4f6
piyush97 <me@piyushmehta.com>
2022-01-22 00:16:32 -0500

    [WIP] categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+import { CATEGORIES } from "../utils/constants";
+import CategoryItem from "./CategoryItem";
+
+const Container = styled.div`
+  display: flex;
+  padding: 20px;
+  justify-content: space-between;
+`;
 
 const Categories = () => {
-  return <div></div>;
+  return (
+    <Container>
+      {CATEGORIES.map((item) => (
+        <CategoryItem item={item} />
+      ))}
+    </Container>
+  );
 };
 
 export default Categories;

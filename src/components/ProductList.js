6955c73aface60bbdddd8e9ee1bc85539efcca2c
piyush97 <me@piyushmehta.com>
2022-02-09 20:14:46 -0500

    responsive

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { mobile } from "../utils/responsive";
 import Announcement from "./Announcement";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
@@ -16,16 +17,19 @@ const FilterContainer = styled.div`
 `;
 const Filter = styled.div`
   margin: 20px;
+  ${mobile({ width: "0px 20px", display: "flex", flexDirection: "column" })}
 `;
 const FilterText = styled.span`
   font-size: 20px;
   font-weight: 600;
   margin-right: 20px;
+  ${mobile({ marginRight: "0px" })}
 `;
 
 const Select = styled.select`
   padding: 10px;
   margin-right: 20px;
+  ${mobile({ margin: "10px 0px" })}
 `;
 const Option = styled.option``;
 const ProductList = () => {

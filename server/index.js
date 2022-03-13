c9a61cfbf099a3acb978d94aae53984e38400fbb
Piyush Mehta <me@piyushmehta.com>
2022-03-12 22:41:17 -0500

    db connection

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+require("dotenv").config();
 
+mongoose
+  .connect(
+    `mongodb+srv://piyush:${process.env.DB_PASS}@cluster0.xulnx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("Db connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
